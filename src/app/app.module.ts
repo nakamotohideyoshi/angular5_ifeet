d9a5a8aa75448749befa4df5ab8eed4eb3620b85
Nakamoto <nakamoto.guru.0991@gmail.com>
2018-04-22 10:18:45 -0700

    import ngx module for camera integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { WebviewDirective } from './directives/webview.directive';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import {WebcamModule} from 'ngx-webcam';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -33,6 +34,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   ],
   imports: [
     BrowserModule,
+    WebcamModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
